8a95e618a3b41e6dc4e5d0f91e9f4bb92a2ddb74
Koku294 <90713556+Koku294@users.noreply.github.com>
2024-12-25 22:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function isSameType(value1, value2) {
   }
 	return typeof value1 === typeof value2;
 }
-}
+
 console.log(isSameType(5, 3));        // true
 console.log(isSameType("hey", "hello")); // true
 console.log(isSameType(NaN, NaN));    // true
@@ -13,6 +13,8 @@ console.log(isSameType("3", 3));      // false
 
 
 // do not change the code below.
+let intValue1 = parseInt(value1);
+let intValue2 = parseInt(value2);
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
